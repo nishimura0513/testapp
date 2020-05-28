2df17ecc32a18ce545ae8a9f35ab84d645d9cb7e
nishimura0513 <63397194+nishimura0513@users.noreply.github.com>
2020-05-28 10:18:15 +0900

    gemアップデート

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,7 @@ class User < ApplicationRecord
   validates :name,{presence: true, uniqueness: true}
   validates :email, {presence: true, uniqueness: true}
 
-  def user
-    return User.find_by(id: self.user_id)
-  end
-
-  def guser
-    return Guser.find_by(id: self.guser_id)
-  end
+  has_many :post
 
 
 end
